03456629be3493dcda45a1fb26e72bbed6522f3e
Tiago Luis Souza Pantoja <tiagopantoja1998@gmail.com>
2023-04-25 18:09:02 -0300

    Adicionando arquivos principais do projeto

---
+++ fatorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+  
+def test_factorial():
+    assert factorial(1) == 1
+
+if __name__ == '__main__':
+   
+    nconditions = raw_input("Please enter number of conditions: ")
+    norders = factorial(nconditions)
+    print("Number of possible trial orders: " + str(norders)
